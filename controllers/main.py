444e25858075bd77c82819acd3636c525be62625
Shawn Rancatore <shawn.rancatore@gmail.com>
2015-02-21 10:27:49 -0500

    clean up

--- controllers/main.py
+++ controllers/main.py
@@ -1,16 +1,27 @@
 import webapp2
 import json
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 class HelloWorld(webapp2.RequestHandler):
     def get(self):
         self.response.write("Hello Liu's world!!")
+
+class Index(webapp2):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
 
 class EndPoint1(webapp2.RequestHandler):
     def get(self):
         self.response.write(json.dumps({'test':['1','two']}))
 
 app = webapp2.WSGIApplication([
-    ('/', HelloWorld)
+    ('/', HelloWorld),
+    ('/endpoints/',EndPoint1)
 ], debug=True)
-
-endpoints = webapp2.WSGIApplication([('/endpoints/',EndPoint1)], debug=True)
-
