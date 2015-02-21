e8fd53534975d7404e9c1772a8b7305e07597f5e
gmisek <gmisek@gmail.com>
2015-02-21 15:36:42 -0500

    Hooking up donor list

--- scripts/donorList.js
+++ scripts/donorList.js
@@ -48,86 +48,65 @@
 	__webpack_require__(4);
 
 	var React = __webpack_require__(8),
-		Jobs = __webpack_require__(2);
+	    DonorList = __webpack_require__(3);
 
-	React.render(React.createElement(Jobs, null), document.getElementById('jobs-container'));
+	React.render(React.createElement(DonorList, null), document.getElementById('app-container'));
+
+
 
 /***/ },
 /* 1 */,
-/* 2 */
+/* 2 */,
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var _ = __webpack_require__(11),
-		$ = __webpack_require__(12),
-		React = __webpack_require__(8);
+	    $ = __webpack_require__(12),
+	    React = __webpack_require__(8);
 
-
-	var Jobs = React.createClass({displayName: "Jobs",
-		getInitialState: function() {
-			return {
-				jobs: []
-			};
-		},
-		componentWillMount: function() {
-			// this.getJobsAjax();
-		},
-		render: function() {
-			return (
-				React.createElement("div", null, 
-					React.createElement("ul", null, 
-						this.renderJobsList(this.state.jobs)
-					)
-				)
-			);
-		},
-		componentDidMount: function() {
-			this.getJobsAjax();
-		},
-		renderJobsList: function(jobs) {
-			return _.map(jobs, function(job) {
-				// {job.status}
-				// {job.accepted_by_phone}
-				// {job.instructions}
-				// {job.contact_phone}
-				// {job.accepted_by_name}
-				// {job.description}
-				// {job.should_notify_donor}
-				// {job.timeframe_start}
-				// {job.timeframe_end}
-				// {job.drop_off_phone}
-				// {job.is_okay_to_text}
-				// {job.donor_name}
-				// {job.address}
-				// {job.drop_off_location}
-				// {job.is_truck_required}
-				// {job.drop_off_name}
-
-				return (
-					React.createElement("li", {className: "list-unstyled"}, 
-						job.status
-					)
-				);
-			}, this);
-		},
-		expandCard: function() {
-		},
-		getJobsAjax: function() {
-			$.get('/job/list')
-			.done(this.getJobsDone)
-			.fail(function(err) {
-				console.log(err);
-			});
-		},
-		getJobsDone: function(data) {
-			var parsedData = JSON.parse( data );
-			this.setState({jobs: parsedData});
-		}
+	var DonorList = React.createClass({displayName: "DonorList",
+	    getInitialState: function() {
+	    		     return {
+			     	    donors: []
+			     };
+	    },
+	    componentWillMount: function() {
+	    			this.getDonorsAjax();			
+	    },
+	    render: function() {    
+	    	    return (
+		    	   React.createElement("div", null, 
+				React.createElement("ul", null, 
+				this.renderDonorsList(this.state.donors)
+				)	    	   
+			   )
+		    );
+	    },
+	    renderDonorsList: function(donors) {
+	    		      return _.map(donors, function(donor) {
+			      	     return (
+				     	    React.createElement("li", {className: "list-unstyled"}, 
+					    	donor.name, " - ", donor.phone
+					    )
+				     );
+			      }, this);
+	    },
+	    getDonorsAjax: function() {
+	    		   $.post('/donor/')
+				.done(this.getDonorsDone)
+				.fail(function(err) { 
+					console.log(err); 
+			   });
+	    },
+	    getDonorsDone: function(data) {
+	    		   var parsedData = JSON.parse(data);
+			   this.setState({donor: parsedData});
+	    }
 	});
 
-	module.exports = Jobs;
+	module.exports = DonorList;
 
 /***/ },
-/* 3 */,
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
