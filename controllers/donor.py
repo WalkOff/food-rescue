b36b778fc19bc7a2c7def5afb05ad0e4a02bd663
gmisek <gmisek@gmail.com>
2015-02-21 14:15:28 -0500

    Added route and file for donor editing

--- controllers/donor.py
+++ controllers/donor.py
@@ -19,6 +19,17 @@ class Index(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'   
         self.response.out.write(json.dumps(donors))
 
+class CreateEdit(webapp2.RequestHandler):
+    def get(self, donor_id):
+        template = JINJA_ENVIRONMENT.get_template('donor_edit.html')
+        self.response.write(template.render())
+    def post(self):
+        donor_id = self.request.get('donorId')
+        donor = donor_id.get()
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(donor))
+
 app = webapp2.WSGIApplication([
-    ('/donor/',Index)
+    ('/donor/?',Index),
+    ('/donor/(\S+)/?',CreateEdit)
 ], debug=True)
