67f275c6883e5b5ac6130a748b312703ca98ccc6
Shawn Rancatore <shawn.rancatore@gmail.com>
2015-02-21 13:00:44 -0500

    some seeding action

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,5 +1,8 @@
 import webapp2
-from seed_data import *
+from models.job import Job
+from models.common import *
+from models.donor import Donor
+from datetime import datetime
 
 class Seed(webapp2.RequestHandler):
     def get(self):
@@ -9,7 +12,20 @@ class Seed(webapp2.RequestHandler):
             donor.put()
         self.response.write("Success")
 
+class MakeJob(webapp2.RequestHandler):
+    def get(self):
+        address = Address(address1="123 Street Dr", address2="Floor 2", city="Pittsburgh",state="PA", zipcode="15239")
+        donor = Donor(name="good will", address=address,phone="3379620553")
+        donor.put()
+        job = Job(donor_id=donor.key, donor_name=donor.name, donor_address=donor.address,timeframe_start=datetime.now(),is_okay_to_text=True,
+                  should_notify_donor=True, is_truck_required=True, contact_phone='123-123-1234', description='awesome job',
+                  instructions='instructions0', timeframe_end=datetime.now(), status=JobStatus.submitted)
+        job.put()
+        self.response.write('done')
+
+
 app = webapp2.WSGIApplication([
-    ('/admin/seed', Seed)
+    ('/admin/seed', Seed),
+    ('/admin/make_job', MakeJob)
 ], debug=True)
 
