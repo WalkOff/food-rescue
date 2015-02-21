0434154cc29a1123205dd3d28a3e3aa2fdd69fd3
Shawn Rancatore <shawn.rancatore@gmail.com>
2015-02-21 16:57:43 -0500

    flesh out admin controller

--- controllers/job_admin.py
+++ controllers/job_admin.py
@@ -5,18 +5,39 @@ from models.job import Job
 from models.common import *
 from models.donor import *
 from models.drop_off import *
+import json
+import jinja2
+from common.helpers import dict_maker
 from seed_data import *
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader("./views/admin"),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-class JobAdmin(BaseHandler):
+class JobList(BaseHandler):
     def get(self):
-        return
+        template = JINJA_ENVIRONMENT.get_template('job_list.html')
+        self.response.write(template.render())
+    def post(self):
+        jobs = Job.query().fetch()
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps([dict_maker(j) for j in jobs]))
+
+class JobDetails(BaseHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('job_view.html')
+        self.response.write(template.render())
+
+class AssignDropOff(BaseHandler):
     def post(self):
         return
+
 
 config = {}
 config['webapp2_extras.sessions'] = {'secret_key': 'secret-session-key-123'}
 
 app = webapp2.WSGIApplication([
-    ('/admin/job', Seed)
+    ('/admin/job', JobDetails),
+    ('/job/?',JobList)
 ], config=config, debug=True)
 
