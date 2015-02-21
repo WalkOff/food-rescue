7d8e11d47ddd3d69bd254c20c35a0a47553e275c
Shawn Rancatore <shawn.rancatore@gmail.com>
2015-02-21 10:00:06 -0500

    Sorry Jenny

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HelloWorld(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello Nathan's World!")
+        self.response.write("Hello Liu's world!")
 
 app = webapp2.WSGIApplication([
     ('/', HelloWorld)
