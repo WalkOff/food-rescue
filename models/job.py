52feb2c2616c434e0c6088f74d0abb8ededa0630
Nathan Cochran <NathanJCochran@gmail.com>
2015-02-21 11:46:20 -0500

    Updated job model

--- models/job.py
+++ models/job.py
@@ -1,8 +1,12 @@
-import common
+from common import *
 from google.appengine.ext import ndb
 
 
 class Job(ndb.Model):
+    # Status of the job:
+    status = msgprop.EnumProperty(JobStatus, required=True)
+
+    # Provided by donor form:
     donor_id = ndb.KeyProperty(kind="Donor", required=True)
     description = ndb.StringProperty(required=True)
     instructions = ndb.StringProperty(required=True)
@@ -14,5 +18,11 @@ class Job(ndb.Model):
     is_okay_to_text = ndb.BooleanProperty(required=True)
     should_notify_donor = ndb.BooleanProperty(required=True)
     is_truck_required = ndb.BooleanProperty(required=True)
+
+    # Once drop off location is assigned:
+    drop_off_id = ndb.KeyProperty(kind="DropOff" required=False)
+    drop_off_location = ndb.StructuredProperty(Address, required = False)
+
+    # Once accepted by a driver:
     is_accepted = ndb.BooleanProperty(required=True)
-    accepted_by = ndb.KeyProperty(kind="Driver")
+    accepted_by = ndb.KeyProperty(kind="Driver", required=False)
