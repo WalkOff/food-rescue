6d157e3f8ddc08ad5c61bd0c04b1b9e71dfa6dd0
Jessie Schalles <jessieschalles@gmail.com>
2015-02-22 01:20:15 -0500

    rm extra nav

--- _static_assets/scripts/components/App.jsx
+++ _static_assets/scripts/components/App.jsx
@@ -13,8 +13,6 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-        <Nav />
-
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
