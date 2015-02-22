4e437834fac6e414a976b935834398a725a7a881
Jessie Schalles <jessieschalles@gmail.com>
2015-02-21 23:11:16 -0500

    init touch

--- _static_assets/scripts/driver-list-entry.js
+++ _static_assets/scripts/driver-list-entry.js
@@ -4,5 +4,6 @@ require('../styles/main.css');
 var React = require('react'),
     DriverList = require('./components/DriverList.jsx');
 
+React.initializeTouchEvents(true);
 React.render(<DriverList />, document.getElementById('app-container'));
 
