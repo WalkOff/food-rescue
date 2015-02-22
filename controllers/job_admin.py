a101c30209b69630818b6c29d46414aebd62d7ec
gmisek <gmisek@gmail.com>
2015-02-21 21:51:44 -0500

    add twilio refs

--- controllers/job_admin.py
+++ controllers/job_admin.py
@@ -9,6 +9,7 @@ import json
 import jinja2
 from common.helpers import dict_maker
 from seed_data import *
+from twilio.rest import TwilioRestClient 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader("./views/admin"),
     extensions=['jinja2.ext.autoescape'],
@@ -33,14 +34,25 @@ class JobDetails(BaseHandler):
 
 class AssignDropOff(BaseHandler):
     def post(self):
-        job_id = ndb.Key(self.request.get('jobId'))
+        job_id = ndb.Key(urlsafe=self.request.get('jobId'))
+        drop_off_id = ndb.Key(urlsafe=self.request.get('dropoffId'))
         job = job_id.get()
-        drop_off_id = ndb.Key(self.request.get('dropoffId'))
-        drop_off = DropOff()
         drop_off = drop_off_id.get()
         job.drop_off_location = drop_off.address
         job.drop_off_name = drop_off.name
         job.drop_off_id = drop_off.key
+        job.put()
+     
+        # TWILIO
+        ACCOUNT_SID = "ACd8d7d3408818d4457f0936cce8229810" 
+        AUTH_TOKEN = "77a8794db8e422eff47bb037f21ee9f1"  
+        client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+        client.messages.create(
+            to="4044466348", 
+            from_="+14124263647", 
+            body="TEST")
+        # /TWILIO
+
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(job))
 
