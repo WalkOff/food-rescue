f7068596c2ca2350c88d62bcbe84fd342f368704
Chris Geihsler <chris@geihsler.net>
2015-02-21 23:26:47 -0500

    Tabs to spaces.

--- _static_assets/scripts/components/App.jsx
+++ _static_assets/scripts/components/App.jsx
@@ -1,48 +1,49 @@
 var _ = require('underscore'),
-	$ = require('jquery'),
-	React = require('react'),
-	Nav = require('./Nav.jsx'),
-	Profile = require('./Profile.jsx'),
-	DonorForm = require('./DonorForm.jsx');
+  $ = require('jquery'),
+  React = require('react'),
+  Nav = require('./Nav.jsx'),
+  Profile = require('./Profile.jsx'),
+  DonorForm = require('./DonorForm.jsx');
 
 var App = React.createClass({
-	getInitialState: function() {
-		return {
+  getInitialState: function() {
+    return {
 
-		};
-	},
-	render: function() {
-		return (
-			<div>
-				<Nav />
+    };
+  },
+  render: function() {
+    return (
+      <div>
+        <Nav />
 
-				<div className="container">
-					<div className="row">
-						<div className="col-xs-12">
-							<DonorForm donationRequest={this.state.donationRequest} onFormSubmit={this.submitInfoAjax} />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	},
-	submitInfoAjax: function(donorForm) {
-    	$.post('/job/new',{job: JSON.stringify(donorForm)})
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <DonorForm donationRequest={this.state.donationRequest} onFormSubmit={this.submitInfoAjax} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+  submitInfoAjax: function(donorForm) {
+      $.post('/job/new',{job: JSON.stringify(donorForm)})
         .done(this.getSubmitDone)
-        .fail(function(err) { 
-            console.log(err); 
+        .fail(function(err) {
+            console.log(err);
         });
-	},
+  },
     getSubmitDone: function(data) {
-    	var parsedData = JSON.parse(data);
-		this.setState({donor: parsedData});
+      var parsedData = JSON.parse(data);
+      this.setState({donor: parsedData});
+      window.location.href = '/job'
     },
-	showProfile: function() {
-		console.log('show profile');
-	},
-	editProfile: function() {
-		console.log('edit profile');
-	}
+  showProfile: function() {
+    console.log('show profile');
+  },
+  editProfile: function() {
+    console.log('edit profile');
+  }
 });
 
 module.exports = App;
